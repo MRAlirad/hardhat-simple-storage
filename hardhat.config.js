c7126fa1017ae11c725dd4938f828d7657f5137d
MRAlirad <m.r.alirad75@gmail.com>
2024-02-26 00:37:22 +0330

    hardhat create task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 require('@nomicfoundation/hardhat-verify');
+require('./tasks/block-number')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+	const accounts = await hre.ethers.getSigners();
+	for (const account of accounts)
+		console.log(account.address);
+});
 
 module.exports = {
 	defualtNetwork: 'hardhat',
