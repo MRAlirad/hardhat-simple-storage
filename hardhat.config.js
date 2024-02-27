9bbf960330cb8e515f20add5939b3630ca498e61
MRAlirad <m.r.alirad75@gmail.com>
2024-02-27 21:27:03 +0330

    gas report
    gas reporting using hardhat-gas-reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 require('@nomicfoundation/hardhat-verify');
-require('./tasks/block-number')
+require('./tasks/block-number');
+require('hardhat-gas-reporter');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || '';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
+const COINMARKEYCAP_API_KEY = process.env.COINMARKEYCAP_API_KEY || '';
 
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 	const accounts = await hre.ethers.getSigners();
@@ -31,5 +33,13 @@ module.exports = {
 	solidity: "0.8.24",
 	etherscan: {
 		apiKey: ETHERSCAN_API_KEY,
+	},
+	gasReporter: {
+		enabled: true,
+		outputFile: 'gas-reporter.txt',
+		noColors: true,
+		currency: 'USD',
+		coinmarketcap: COINMARKEYCAP_API_KEY,
+		// token: 'MATIC'
 	}
 };
